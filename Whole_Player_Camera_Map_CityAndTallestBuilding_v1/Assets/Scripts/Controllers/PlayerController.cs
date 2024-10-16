f510a39923090b77da2da41d3927744ab1b90050
soongsilkkh <kkh980729@soongsil.ac.kr>
2024-10-16 14:20:39 +0900

    Update Whole temporary - adjust detail, refactor code

--- Whole_Player_Camera_Map_CityAndTallestBuilding_v1/Assets/Scripts/Controllers/PlayerController.cs
+++ Whole_Player_Camera_Map_CityAndTallestBuilding_v1/Assets/Scripts/Controllers/PlayerController.cs
@@ -25,8 +25,16 @@ public class PlayerController : MonoBehaviour
         OnAir,
 
     }
+    public enum PlayerVerticalMovement
+    {
+        Idle, Forward, Back
+    }
 
-    
+    public enum PlayerHorizontalMovement
+    {
+        Idle, Left, Right
+    }
+
     public enum PlayerMode
     {
         Reality, //현실
@@ -34,36 +42,30 @@ public class PlayerController : MonoBehaviour
         Puzzle, //미니게임
 
     }
+
     
-    
-    public PlayerState StatePlayer=PlayerState.Idle;
-    PlayerAt _at=PlayerAt.OnGround;
-    Rigidbody _rb =null;
+
+    public PlayerState StatePlayer = PlayerState.Idle;
+    public PlayerAt AtPlayer = PlayerAt.OnGround;
+
+    public PlayerVerticalMovement PlayerVertMove = PlayerVerticalMovement.Idle;
+    public PlayerHorizontalMovement PlayerHorizonMove = PlayerHorizontalMovement.Idle;
+
+    public bool JumpLock = false;
+     
+
 
     [SerializeField]
     float _speed = 4.0f;
 
 
-    public enum PlayerVerticalMovement
-    {
-        Idle, Forward, Back
-    }
-    public enum PlayerHorizontalMovement
-    {
-        Idle, Left, Right
-    }
-    public PlayerVerticalMovement PlayerVertMove = PlayerVerticalMovement.Idle;
-    public PlayerHorizontalMovement PlayerHorizonMove = PlayerHorizontalMovement.Idle;
+    Rigidbody _rb = null;
 
 
-    [SerializeField]
-    Camera _camera = null;
-    CameraController _cameraController = null;
 
     private void Start()
     {
         _rb=GetComponent<Rigidbody>();
-        _cameraController=_camera.GetComponent<CameraController>();
 
         Managers.Input.KeyAction -= this.OnKeyBoardJump;
         Managers.Input.KeyAction += this.OnKeyBoardJump;
@@ -86,145 +88,11 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         //마지막 플레이어 위치 _at이 OnAir일때
-        if(_at==PlayerAt.OnAir&&isPlayerOn())
-            _at = PlayerAt.OnGround;
+        if(AtPlayer == PlayerAt.OnAir&&isPlayerOn())
+            AtPlayer = PlayerAt.OnGround;
 
     }
 
-
-    //test
-    bool isEntered = false;
-    float prevX;
-    //맵 시작과 끝 트리거
-    private void OnTriggerEnter(Collider other)
-    {
-        prevX=transform.position.x;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        //완전히 지나갔는지
-        //잠시 건들고 되돌아갔는지
-        //구분
-        float nowX = transform.position.x;
-        float resultX=prevX-nowX;
-        float standard = other.GetComponent<BoxCollider>().size.x;
-
-        Debug.Log($"prevX - nowX {resultX}, wid {standard}");
-
-        //_cameraController._cameraMode = Define.CameraMode.VerticalHumanView;
-
-        //현재는 이름으로 나중엔 태그로?
-        //원래는 전 맵(장소)의 EndTrigger에서 받아야함.
-
-        if (resultX>1.0f||resultX<-1.0f)
-        { 
-            if (other.gameObject.name == "StartTrigger0")
-            {
-                //임시 현재 맵 정보 받기
-
-                _cameraController.StoreMapInfo(h: 8, w: 75,
-                    s: 20, e: 20 + 75);
-
-                Debug.Log("store next map0 info");
-
-                isEntered = true;
-
-            }
-            else if (other.gameObject.name == "EndTrigger0")
-            {
-                if (isEntered)
-                {
-                    _cameraController.StoreMapInfo(h: 8, w: 29,
-                        e: -13 + 29, s: -13);
-                    Debug.Log("store next map1 info");
-                    isEntered = false;
-
-                }
-                else
-                {
-                    _cameraController.StoreMapInfo(h: 8, w: 29,
-                    s: -48, e: -48 + 29);
-                    Debug.Log("store next map0 info");
-                    isEntered = true;
-                }
-
-            }
-            else if (other.gameObject.name == "StartTrigger1")
-            {
-                if (isEntered)
-                {
-                    _cameraController.StoreMapInfo(h: 8, w: 29,
-                        s: -48, e: -48 + 29);
-                    Debug.Log("store next map0 info");
-                    isEntered = false;
-                }
-                else
-                {
-                    _cameraController.StoreMapInfo(h: 8, w: 29,
-                        e: -13 + 29, s: -13);
-                    Debug.Log("store next map1 info");
-                    isEntered = true;
-                }
-
-            }
-            else if (other.gameObject.name == "EndTrigger1")
-            {
-                if (isEntered)
-                {
-                    _cameraController.StoreMapInfo(h: 8, w: 29,
-                        s: 22, e: 22 + 29);
-                    Debug.Log("store next map2 info");
-                    isEntered = false;
-                }
-                else
-                {
-                    _cameraController.StoreMapInfo(h: 8, w: 29,
-                        e: -13 + 29, s: -13);
-                    Debug.Log("store next map1 info");
-                    isEntered = true;
-                }
-            }
-            else if (other.gameObject.name == "StartTrigger2")
-            //원래는 전 맵(장소)의 EndTrigger에서 받아야함.
-            {
-                if (isEntered)
-                {
-                    _cameraController.StoreMapInfo(h: 8, w: 29,
-                        e: -13 + 29, s: -13);
-                    Debug.Log("store next map1 info");
-                    isEntered = false;
-                }
-                else
-                {
-                    _cameraController.StoreMapInfo(h: 8, w: 29,
-                        s: 22, e: 22 + 29);
-                    Debug.Log("store next map2 info");
-                    isEntered = true;
-                }
-
-            }
-            else if (other.gameObject.name == "EndTrigger2")
-            {
-
-                if (isEntered)
-                {
-                    Debug.Log("test end");
-                    isEntered = false;
-                }
-                else
-                {
-                    _cameraController.StoreMapInfo(h: 8, w: 29,
-                        e: -13 + 29, s: -13);
-                    Debug.Log("store next map2 info");
-                    isEntered = true;
-                }
-
-            }
-        }
-    }
-
-    
     
 
     void OnKeyBoardMove()
@@ -299,20 +167,22 @@ public class PlayerController : MonoBehaviour
 
     void OnKeyBoardJump()
     {
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!JumpLock)
         {
-            if (_at == PlayerAt.OnGround)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                _at = PlayerAt.OnAir;
-                _rb.AddForce(Vector3.up * _speed*1.5f, ForceMode.Impulse);
+                if (AtPlayer == PlayerAt.OnGround)
+                {
+                    AtPlayer = PlayerAt.OnAir;
+                    _rb.AddForce(Vector3.up * 6.0f, ForceMode.Impulse);
+                }
             }
-        }
 
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            if(isPlayerOn())
-                _at = PlayerAt.OnGround;
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                if (isPlayerOn())
+                    AtPlayer = PlayerAt.OnGround;
+            }
         }
 
     }
@@ -342,6 +212,13 @@ public class PlayerController : MonoBehaviour
     {
         return Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, 0.2f,layer);
     }
+
+
+
+
+
+
+
 
     string[] _names = Enum.GetNames(typeof(PlayerState));
     void PrintPlayerState()
